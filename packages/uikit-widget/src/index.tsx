2ea08b1737adb800ed39b8ad0118b29fbbbad795
glukavz <glukavz@gmail.com>
2022-09-06 22:00:20 +0200

    chore: add docs

--- packages/uikit-widget/src/index.tsx
+++ packages/uikit-widget/src/index.tsx
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <div>
   <Button color="red" />
-
+  <Button color="green" />
+  <Button color="blue" />
  </div>
 )
 
